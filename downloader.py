47ded66c6da8c47b15344f7997e9f2f1656fc08f
Leo Zhou <glasslion@gmail.com>
2018-09-12 16:14:00 +0800

    more data cleanning for flights df

--- downloader.py
+++ downloader.py
@@ -83,6 +83,48 @@ def download_weather():
 
 
 def download_flights():
+    def read(fp):
+        df = (pd.read_csv(fp)
+                .rename(columns=str.lower)
+                .drop('unnamed: 36', axis=1)
+                .pipe(extract_city_name)
+                .pipe(time_to_datetime, ['dep_time', 'arr_time', 'crs_arr_time', 'crs_dep_time'])
+                .assign(fl_date=lambda x: pd.to_datetime(x['fl_date']),
+                        dest=lambda x: pd.Categorical(x['dest']),
+                        origin=lambda x: pd.Categorical(x['origin']),
+                        tail_num=lambda x: pd.Categorical(x['tail_num']),
+                        unique_carrier=lambda x: pd.Categorical(x['unique_carrier']),
+                        cancellation_code=lambda x: pd.Categorical(x['cancellation_code'])))
+        return df
+
+    def extract_city_name(df):
+        '''
+        Chicago, IL -> Chicago for origin_city_name and dest_city_name
+        '''
+        cols = ['origin_city_name', 'dest_city_name']
+        city = df[cols].apply(lambda x: x.str.extract("(.*), \w{2}", expand=False))
+        df = df.copy()
+        df[['origin_city_name', 'dest_city_name']] = city
+        return df
+
+    def time_to_datetime(df, columns):
+        '''
+        Combine all time items into datetimes.
+
+        2014-01-01,0914 -> 2014-01-01 09:14:00
+        '''
+        df = df.copy()
+        def converter(col):
+            timepart = (col.astype(str)
+                        .str.replace('\.0$', '')  # NaNs force float dtype
+                        .str.pad(4, fillchar='0'))
+            return pd.to_datetime(df['fl_date'] + ' ' +
+                                timepart.str.slice(0, 2) + ':' +
+                                timepart.str.slice(2, 4),
+                                errors='coerce')
+        df[columns] = df[columns].apply(converter)
+        return df
+
     headers = {
     'Referer': 'https://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236&DB_Short_Name=On-Time',
     'Origin': 'https://www.transtats.bts.gov',
@@ -111,9 +153,16 @@ def download_flights():
                     f.write(chunk)
     zf = zipfile.ZipFile("data/flights.csv.zip")
     fp = zf.extract(zf.filelist[0].filename, path='data/')
-    df = pd.read_csv(fp, parse_dates=["FL_DATE"]).rename(columns=str.lower)
+
+    output = 'data/flights.h5'
+    if not os.path.exists(output):
+        df = read(fp)
+        df.to_hdf(output, 'flights', format='table')
+    else:
+        df = pd.read_hdf(output, 'flights', format='table')
     return df
 
 
 def download_all():
     download_weather()
+    download_flights()
